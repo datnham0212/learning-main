b46dc0cb9937fd7d3095a839141da7afd54df013
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-09-12 10:12:14 +0700

    Add files via upload

---
+++ HW3/ex1.py
@@ -0,0 +1,19 @@
+s = 'pythonfile.py'
+
+print(s[2]) #t
+print(s[-1]) #y
+print(len(s)) #13
+print(s[0:7]) #pythonf
+
+print(s[6:10]) #file
+print(s[10:]) #.py
+
+# print(s[::-1])
+
+for i in range(len(s) - 1, -1, -1):
+    print(s[i], end='')
+
+
+
+
+
